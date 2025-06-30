4137244a62f2a7e0534fc50a148dedf9a8c2e737
GabrielR4SH <bieelsantana23@hotmail.com>
2025-06-29 22:30:14 -0300

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+
+    $numbers = [1,2,23,4,5,6,7,8,9];
+    echo count($numbers);
+    
+});
