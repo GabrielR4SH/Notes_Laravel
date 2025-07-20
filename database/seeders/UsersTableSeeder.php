6e020e3df1502254e78dd6498f9c21514c7e8e64
GabrielR4SH <bieelsantana23@hotmail.com>
2025-07-20 14:09:51 -0300

    feat: seeder Users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // create multiple users
+        DB::table('users')->insert([
+            [
+                'username' => 'gabriel@hotmail.com',
+                'password' => bcrypt('Ti@nilda123'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'username' => 'lukas@hotmail.com',
+                'password' => bcrypt('V@v1$123'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'username' => 'gessko@hotmail.com',
+                'password' => bcrypt('sa@ai5454'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+        ]);
+    }
+}
